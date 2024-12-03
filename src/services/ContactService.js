7ef5158588d87d1635db67e58de8fbf4aee8ee63
GanpatHada <hadaganpat42@gmail.com>
2024-12-03 16:30:13 +0530

    fix:env variables

--- src/services/ContactService.js
+++ src/services/ContactService.js
@@ -4,9 +4,6 @@ const templateKey=import.meta.env.VITE_EMAILJS_TEMPLATE_KEY;
 const publicKey=import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
 export async function sendMessage(message){
-    console.log(serviceKey,templateKey,publicKey);
-    console.log(import.meta.env.VITE_EMAILJS_SERVICE_KEY);
-    return ;
     let finalMessage={};
     const{clientNumber}=message;
     if(clientNumber.trim().length===0)
