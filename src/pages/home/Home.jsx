3140203e747cac29419f6fa0d2e672cb3fcbf640
GanpatHada <hadaganpat42@gmail.com>
2025-01-01 00:37:12 +0530

    style : Inprove homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,26 +5,38 @@ const Home = () => {
     <div id="home-page" className="all-centered">
       <div id="home-content">
         <section id="open-to-work">
-            <span id="green-light"></span>
-            <span id="open-to-work-text">Available for opportunities</span>
+          <span id="green-light"></span>
+          <span id="open-to-work-text">Available for opportunities</span>
         </section>
         <section id="intro">
-        <p id="short-intro">
-          I am <span>Ganpat Hada</span> , I design and develop visually
-          appealing and user-friendly webapps
-        </p>
+          <p id="short-intro">
+            I am <span>Ganpat Hada</span> , I design and develop visually
+            appealing and user-friendly webapps
+          </p>
+          <p id="long-intro">
+            With hands-on experience in the MERN stack, I craft seamless user
+            experiences and efficient server-side solutions. Open to learning
+            new technologies and tackling challenging projects.
+          </p>
         </section>
-       <section id="home-action">
-       <a target="_blank" href="https://www.linkedin.com/in/ganpathada/" id="linkedin-button" className="home-buttons">
-          Linkedin
-          
-        </a>
-        <a target="_blank" href="https://github.com/GanpatHada" id="github-button" className="all-centered home-buttons">
-          Github 
-          
-        </a>
-       </section>
-        
+        <section id="home-action">
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/ganpathada/"
+            id="linkedin-button"
+            className="home-buttons"
+          >
+            Linkedin
+          </a>
+          <a
+            target="_blank"
+            href="https://github.com/GanpatHada"
+            id="github-button"
+            className="all-centered home-buttons"
+          >
+            Github
+          </a>
+        </section>
       </div>
     </div>
   );
